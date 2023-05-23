12a2d18c81784d566240e83d7f4c4d2b8d00c47a
Anubisa97 <assamsias97@gmail.com>
2023-05-23 16:05:00 +0300

    Aded exercises

---
+++ 2-exercise/9.js
@@ -0,0 +1,10 @@
+// • The first parameter - lost fights count - is an integer in the range [0, 1000].
+// • The second parameter - helmet price - is the floating-point number in the range [0, 1000].
+// • The third parameter - sword price - is the floating-point number in the range [0, 1000].
+// • The fourth parameter - shield price - is the floating-point number in the range [0, 1000].
+// • The fifth parameter - armor price - is the floating-point number in the range [0, 1000]. 
+
+
+function gladiatorExpenses(lostFights, helmetPrice, swordPrice, shieldPrice, armorPrice) {
+    
+}
