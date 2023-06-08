5aa05d6e21a20f28d20e0f3975978862be1d1094
Anubisa97 <assamsias97@gmail.com>
2023-06-08 03:42:27 +0300

    Sync

--- 4-exercise/1.js
+++
@@ -1,7 +0,0 @@
-function smallestOfThreeNumbers(numOne, numTwo, numThree) {
-    let smallestNumber = Math.min(numOne,numTwo,numThree)
-    return smallestNumber
-}
-console.log(smallestOfThreeNumbers(2,
-    5,
-    1));
